019f8f814cc566342db228a55c175280fba16860
Rafshik <artem.samsonow25@yandex.ru>
2024-01-30 19:22:45 +0300

    Project настройка

--- vite.config.js
+++ vite.config.js
@@ -5,25 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/test_project2',
-  build: {
-    rollupOptions: {
-      input: './src/App.jsx',
-    },
-  },
-  optimizeDeps: {
-    include: [
-      '@reduxjs/toolkit',
-      'axios',
-      'react',
-      'react-dom',
-      'react-hook-form',
-      'react-redux',
-      'react-router-dom',
-      '@types/react',
-      '@types/react-dom',
-    ],
-  },
-  server: {
-    port: 8080,
-  },
 })
