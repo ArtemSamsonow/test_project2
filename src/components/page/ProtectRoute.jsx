663fca6755d9c23035adb705a280e96f91dddb16
Rafshik <artem.samsonow25@yandex.ru>
2024-01-30 20:33:58 +0300

    Project настройка

--- src/components/page/ProtectRoute.jsx
+++ src/components/page/ProtectRoute.jsx
@@ -1,7 +1,7 @@
 
 export const ProtectRoute = ({element}) => {
     if (document.cookie.indexOf("token") !== 0){
-        return window.location.replace('/login')
+        return window.location.replace('/test_project2/login')
     }
     else {
         return element
